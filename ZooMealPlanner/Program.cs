f0c20a5c88db99ae333735a2c4ea2e2ba08f1f80
Alex Chen <alexziqic@gmail.com>
2019-11-19 13:57:55 -0800

    Add project files.

---
+++ ZooMealPlanner/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ZooMealPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======================================");
+            Console.WriteLine();
+            Console.WriteLine("Zoo Menu Planner");
+            Console.WriteLine();
+            Console.WriteLine("======================================");
+            Console.WriteLine("1. Monkey");
+            Console.WriteLine();
+            Console.WriteLine("2. Bear");
+            Console.WriteLine();
+            Console.WriteLine("3. Quit");
+            Console.WriteLine();
+            Console.WriteLine("Selection:");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
