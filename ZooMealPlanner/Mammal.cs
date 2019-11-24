ccbad711f8ebb30dec17c917bafcb78457d2e5ab
Alex Chen <alexziqic@gmail.com>
2019-11-24 15:32:15 -0800

    more comments and adjustments of number of digits added

--- ZooMealPlanner/Mammal.cs
+++ ZooMealPlanner/Mammal.cs
@@ -19,14 +19,17 @@ namespace ZooMealPlanner
             Species = species;
         }
 
-
         //the final outout function
-        public void printdetails(double inputweight)
+        public void printdetails(double inputWeight)
         {
+            string inputWeightString = inputWeight.ToString("F");//adjustment of number of digits
+            double servingKg = ServingRatio * inputWeight;//calculation of serving amount
+            string servingKgString = servingKg.ToString("0.000");//adjustment of number of digits
+
             Console.WriteLine($"Mammal Type:  {Type}");
             Console.WriteLine($"Species:      {Species}");
-            Console.WriteLine($"Weight:       {inputweight}KG");
-            Console.WriteLine($"Serving:      {ServingRatio * inputweight}KG {FoodPreference}");
+            Console.WriteLine($"Weight:       {inputWeightString}KG");
+            Console.WriteLine($"Serving:      {servingKgString}KG {FoodPreference}");
             Console.WriteLine("Instructions: Keep area secure at all times.");
             Console.WriteLine($"              Feed at {FeedTime}");
             
